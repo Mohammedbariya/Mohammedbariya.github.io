913b17ec18dc6148df9b4e1cf8d6d0f8c9fa3c66
Mohammed <mohammedbariyawala752@gmail.com>
2023-09-13 16:17:32 +0530

    added my website files in this folder

---
+++ js/resp.js
@@ -0,0 +1,31 @@
+let burger = document.getElementById('burger');
+let rightnav = document.getElementById('rightnav');
+burger.addEventListener('click', () => {
+    if (rightnav.style.display == "none") {
+        rightnav.style.display = "block";
+        rightnav.style.animation = "burgeranimation 2s";
+        // rightnav.style.opacity = 1;
+    } else {
+        rightnav.style.display = "none";
+        // rightnav.style.opacity = 0;
+    }
+});
+
+let resumenav = document.getElementById('resumenav');
+let resumenavlist = document.getElementsByClassName("resumenavlist")
+// let resumenavlist = document.getElementById('resumenavlist');
+let resumenavtoggle = document.getElementById('resumenavtoggle');
+
+
+resumenav.addEventListener('click', () => {
+    if (resumenavlist[0].style.display == "none") {
+        for (let index = 0; index < resumenavlist.length; index++) {
+            resumenavlist[index].style.display = "block";
+        }
+
+    } else {
+        for (let index = 0; index < resumenavlist.length; index++) {
+            resumenavlist[index].style.display = "none";
+        }
+    }
+});
---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+// function changenav(){
+//     var scrollvalue = window.scrollY;
+    
+
+//     console.log(scrollvalue);
+// }
+
+// window.addEventListener('scroll',changenav);
