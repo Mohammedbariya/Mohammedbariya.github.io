bacdb22d03ae155fd03f97f4add6c54a0030b2a6
Mohammed <mohammedbariyawala752@gmail.com>
2023-09-14 14:52:18 +0530

    decresed typing speed

--- js/typing.js
+++ js/typing.js
@@ -2,6 +2,7 @@ let element = document.getElementById('element');
 const words = ['Web Developer', 'Fast Learner', 'Problem Solver', 'Software Engineer'];
 
 let currentIndex = 0;
+let speed = 200
 
 function startTypingLoop() {
     if (currentIndex >= words.length) {
@@ -12,14 +13,13 @@ function startTypingLoop() {
     typing_animation(currentIndex);
     currentIndex++;
 
-    setTimeout(startTypingLoop, words[currentIndex - 1].length * 130 + 1000); // Delay based on word length
+    setTimeout(startTypingLoop, words[currentIndex - 1].length * speed + 1000); // Delay based on word length
 }
 
 
 function typing_animation(num){
     var word = words[num];
     var index = 0;
-    var speed = 130;
     function type_word(){
         if (index <=word.length){
             element.innerHTML = word.substring(0, index) + '<span class="cursor">|</span>';
