5b40c38aa5ebd159a7f186b90fa4401157e249f4
Mohammed <mohammedbariyawala752@gmail.com>
2023-09-25 16:51:42 +0530

    added active state for nav bar

---
+++ js/nav.js
@@ -0,0 +1,25 @@
+const sections = document.querySelectorAll('section');
+const navli = document.querySelectorAll('.nav-link');
+const homenav = document.getElementById('homenav');
+const firstsection = document.getElementById('firstsection');
+
+window.addEventListener('scroll',()=>{
+    let current = '';
+    sections.forEach(section =>{
+        const sectiontop = section.offsetTop;
+        const sectionheight = section.clientHeight;
+        if(scrollY >= sectiontop){
+            current = section.getAttribute('id');
+        }
+    });
+    console.log(current);
+    navli.forEach( li =>{
+        li.classList.remove('active');
+        if(li.classList.contains(current)){
+            li.classList.add('active');
+        }
+        if(scrollY < firstsection.offsetTop){
+            homenav.classList.add('active');
+        }
+    });
+});
