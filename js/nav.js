d3a5614a7db28cb94f09ce2962cebc8ef0cccbc8
Mohammed <mohammedbariyawala752@gmail.com>
2023-09-26 10:17:12 +0530

    added active navbar for resumesection also

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,4 @@
+// for main nav bar
 const sections = document.querySelectorAll('section');
 const navli = document.querySelectorAll('.nav-link');
 const homenav = document.getElementById('homenav');
@@ -20,6 +21,33 @@ window.addEventListener('scroll',()=>{
         }
         if(scrollY < firstsection.offsetTop){
             homenav.classList.add('active');
+        }
+    });
+});
+
+// for resumesection nav bar
+const resume_a = document.querySelectorAll('.page');
+const resumenavli = document.querySelectorAll('.resumenavlist');
+const resumefirstnav = document.getElementById('page0');
+const firstpage = document.getElementById('page-0');
+
+window.addEventListener('scroll',()=>{
+    let current = '';
+    resume_a.forEach(resume =>{
+        const atop = resume.offsetTop;
+        const aheight = resume.clientHeight;
+        if(scrollY >= (atop - aheight/3)){
+            current = resume.getAttribute('id');
+        }
+    });
+    // console.log(current);
+    resumenavli.forEach( a =>{
+        a.classList.remove('active');
+        if(a.classList.contains(current)){
+            a.classList.add('active');
+        }
+        if(scrollY < firstpage.offsetTop){
+            resumefirstnav.classList.add('active');
         }
     });
 });
--- js/resp.js
+++ js/resp.js
@@ -1,35 +1,39 @@
-let burger = document.getElementById('burger');
-let rightnav = document.getElementById('rightnav');
-let main = document.getElementById('main')
+// Use window.matchMedia with the correct media query format
+let screensize = window.matchMedia("(max-width: 1200px)");
 
-burger.addEventListener('click', () => {
-    if (rightnav.style.display == "none") {
-        rightnav.style.display = "block";
-        rightnav.style.animation = "burgeranimation 2s";
-        // rightnav.style.opacity = 1;
+if (screensize.matches) {
+  // Wrap your code inside this condition to only execute it when the media query matches
+
+  let burger = document.getElementById("burger");
+  let rightnav = document.getElementById("rightnav");
+  let main = document.getElementById("main");
+
+  burger.addEventListener("click", () => {
+    if (rightnav.style.display === "none" || rightnav.style.display === "") {
+      rightnav.style.display = "block";
+      rightnav.style.animation = "burgeranimation 2s";
     } else {
-        rightnav.style.display = "none";
-        // rightnav.style.opacity = 0;
+      rightnav.style.display = "none";
     }
-});
-main.addEventListener('click',()=>{
+  });
+
+  main.addEventListener("click", () => {
     rightnav.style.display = "none";
-});
+  });
 
-let resumenav = document.getElementById('resumenav');
-let resumenavlist = document.getElementsByClassName("resumenavlist")
-let resumenavtoggle = document.getElementById('resumenavtoggle');
+  let resumenav = document.getElementById("resumenav");
+  let resumenavlist = document.getElementsByClassName("resumenavlistjs");
+  let resumenavtoggle = document.getElementById("resumenavtoggle");
 
-
-resumenav.addEventListener('click', () => {
-    if (resumenavlist[0].style.display == "none") {
-        for (let index = 0; index < resumenavlist.length; index++) {
-            resumenavlist[index].style.display = "block";
-        }
-
+  resumenav.addEventListener("click", () => {
+    if (resumenavlist[0].style.display === "none" || resumenavlist[0].style.display === "") {
+      for (let index = 0; index < resumenavlist.length; index++) {
+        resumenavlist[index].style.display = "block";
+      }
     } else {
-        for (let index = 0; index < resumenavlist.length; index++) {
-            resumenavlist[index].style.display = "none";
-        }
+      for (let index = 0; index < resumenavlist.length; index++) {
+        resumenavlist[index].style.display = "none";
+      }
     }
-});
+  });
+}
